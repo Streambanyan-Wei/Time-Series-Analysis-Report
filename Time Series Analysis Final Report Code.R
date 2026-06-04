d0f540be18ecceebea190504e6a2df2e90ae872e
Streambanyan-Wei <xirong2005@gmail.com>
2026-06-04 11:07:30 +0930

    Update Time Series Analysis Final Report Code.R
    Add first modelling part TFR

--- Time Series Analysis Final Report Code.R
+++ Time Series Analysis Final Report Code.R
@@ -39,4 +39,42 @@ TFRa<-fertility_train$TFR %>% ts(start = 1960)
 
 plot(TFRa)
 
+#Modelling####
+## Model TFR ####
+#Check the p value of TFR
+unitroot_kpss(TFRa)
+#Since the p value is less than 0.05, reject the hypothesis
+
+#Check the first difference of the TFR which aims to remove trend
+plot(diff(TFRa))
+unitroot_kpss(diff(TFRa))
+#Obviously diff(TFRa) isn't stationary, because the p value is still less than 0.05
+
+#Try to check the ACF of the TFR
+acf(diff(TFRa),lag=45)
+#ACF has a spike on lag8
+#Double check PACF
+pacf(diff(TFRa),lag=45)
+#PACF has a spike on lag16, so try to model with ARIMA(16,1,0)
+plot(arima(TFRa,order=c(16,1,0))$resid,lag=45) 
+plot(acf(arima(TFRa,order=c(16,1,0))$resid,lag=45))
+plot(pacf(arima(TFRa,order=c(16,1,0))$resid,lag=45))
+arima(TFRa,order=c(16,1,0))
+
+#The residual is the white noise
+#Try another model
+#ARIMA(16,1,1)
+plot(acf(arima(TFRa,order=c(16,1,1))$resid,lag=45))
+plot(pacf(arima(TFRa,order=c(16,1,1))$resid,lag=45))
+arima(TFRa,order=c(16,1,1))
+
+plot(acf(arima(TFRa,order=c(15,1,1))$resid,lag=45))
+plot(pacf(arima(TFRa,order=c(15,1,1))$resid,lag=45))
+arima(TFRa,order=c(15,1,1))
+
+plot(acf(arima(TFRa,order=c(15,1,0))$resid,lag=45))
+plot(pacf(arima(TFRa,order=c(15,1,0))$resid,lag=45))
+arima(TFRa,order=c(16,1,1))
+#Have significent spike on lag16,reject
+
 
